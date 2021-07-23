225837bf58c5e56377ffc4a9684491d8ced097f0
EU3 Batch GitHup <msen2020@hotmail.com>
2021-07-23 20:50:18 +0200

    add trial

--- src/Deneme.java
+++ src/Deneme.java
@@ -2,7 +2,10 @@
 public class Deneme {
 	
 	public static void main(String[] args) {
-		System.out.println("1. Satýr - Hamza");
+		System.out.println("1. Satï¿½r - Hamza");
 	}
-
+Public void Trial(){
+		Trial();
+		
+	}
 }
