934d4d7f57537c93a26d720cb16955562f9f5274
EU3 Batch GitHup <msen2020@hotmail.com>
2021-07-23 21:01:48 +0200

    v000

--- src/Deneme.java
+++ src/Deneme.java
@@ -4,5 +4,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("1. Satir - Hamza");
 		System.out.println("2. Satir - Mehmet");
+		System.out.println("3. Satir - Mehmet");
+
 	}
 }
