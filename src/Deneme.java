f271b9b2bbe4257f564fcbab9ca1aa7697d18f96
EU3 Batch GitHup <msen2020@hotmail.com>
2021-07-23 20:41:49 +0200

    Add Mehmet

--- src/Deneme.java
+++ src/Deneme.java
@@ -2,7 +2,9 @@
 public class Deneme {
 	
 	public static void main(String[] args) {
-		System.out.println("1. Satýr - Hamza");
+		System.out.println("1. Satï¿½r - Hamza");
+		System.out.println("1. Satï¿½r - Mehmet");
+
 	}
 
 }
