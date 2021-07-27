73b245d3d09c38303e51b1d8b48c8e25c6f1da3f
EU3 Batch GitHup <msen2020@hotmail.com>
2021-07-27 12:47:05 +0200

    007

--- src/Deneme.java
+++ src/Deneme.java
@@ -3,14 +3,23 @@ public class Deneme {
 	
 	public static void main(String[] args) {
 		System.out.println("1. Satir - Hamza");
-		System.out.println("2. Satir - Mehmet");
+		System.out.println("2. Satir - Mustafa");
 		System.out.println("3. Satir - Mehmet");
 		System.out.println("How can I open my project from Git CMD?");
+		
 		System.out.println("git add . ");
-		System.out.println("git commit -m "v005");
+		System.out.println("git commit -m "v005");"
+				+ "
 		System.out.println("git log --> commit list");
 		System.out.println("git status");
 		System.out.println("git push");
-		System.out.println("");
+		
+		System.out.println("git fetch");
+		System.out.println("git merge");
+		System.out.println("git pull");// v006
+		System.out.println("git branch");
+		System.out.println("git branch hamza");
+		System.out.println("git merge hamza");
+		System.out.println("git checkout hamza");// v007
 	}
 }
